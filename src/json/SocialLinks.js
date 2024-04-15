4e2686e93463258a7379963e787e211a8fcf5252
Panji <panji9502@gmail.com>
2024-04-15 16:22:35 +0700

    Update frontend mentor url

--- src/json/SocialLinks.js
+++ src/json/SocialLinks.js
@@ -1,7 +1,7 @@
 let socialLinks = [
   {
     github: "https://github.com/pperdana",
-    frontendMentor: "https://www.frontendmentor.io/profile/Panji200",
+    frontendMentor: "https://www.frontendmentor.io/profile/pperdana",
     email: "panjiperdana088@gmail.com",
     youtube: "https://www.youtube.com",
   },
